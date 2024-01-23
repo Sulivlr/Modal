d72d210dfcc007795fa40667cf15eef70c4f4c80
Sultan Mukhtarov <suli419956@gmail.com>
2024-01-23 16:51:05 +0600

    В качестве стандартной верстки я взял предыдущую домашку и добавил туда модалку с классной работы. Далее добавил кнопку с классом btn-close и таким образом появилась кнопка закрыть. Alert не правильно получался, не стал закидывать.

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import UserForm from "./components/UserForm/UserForm";
+import Users from "./components/Users/Users";
+import {useState} from "react";
+import {User} from "../src/components/types";
+
+
+function App() {
+
+    const [users, setUsers] = useState<User[]>([]);
+
+    const addUser = (user: User) => {
+        setUsers(prevState => [...prevState, user])
+    };
+
+    return (
+        <>
+            <main className="container-fluid">
+                <div className="row mt-2">
+                    <div className="col-4">
+                        <UserForm onSubmit={addUser}/>
+                    </div>
+                    <div className="col-4">
+                        <Users users={users}/>
+                    </div>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default App
